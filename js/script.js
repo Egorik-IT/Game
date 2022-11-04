a8bdafb55c5cff200ff9fdb4bc7383611103f37a
Egorik-IT <113302302+Egorik-IT@users.noreply.github.com>
2022-11-04 13:44:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function closed(){
+    document.querySelector(".info").style.right = "-1000px"
+} 
+function info(){
+    document.querySelector(".info").style.right = "62px"
+    document.querySelector(".info__close").style.bottom = "174px"
+}
+   
+
